d974772ae018ec76647568e23a9eca4f3538d5c9
Federico Fissore <federico@fissore.org>
2019-04-09 15:05:09 +0200

    docker: also releasing 'latest'

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -3,7 +3,9 @@
 set -ex
 
 docker build -t ffissore/shorty:$TRAVIS_TAG .
+docker tag ffissore/shorty:$TRAVIS_TAG ffissore/shorty:latest
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker push ffissore/shorty
+docker push ffissore/shorty:$TRAVIS_TAG
+docker push ffissore/shorty:latest
