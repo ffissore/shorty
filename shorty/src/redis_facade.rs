eee9bc88bf95a1b151ed1121cd7596971226d718
Federico Fissore <federico@fissore.org>
2019-04-09 16:13:21 +0200

    Using newtype in redis_facade

--- shorty/src/redis_facade.rs
+++ shorty/src/redis_facade.rs
@@ -20,37 +20,35 @@ use redis::{Connection, RedisResult};
 /// `RedisFacade` is a wrapper around a `redis` `Connection`. It provides convenience methods such
 /// as `get_string` and `get_bool` which otherwise would be coded as `get::<_, String>` and
 /// `get::<_, bool>`, making it harder to stub the struct and properly test `shorty`.
-pub struct RedisFacade {
-    redis: Connection,
-}
+pub struct RedisFacade(Connection);
 
 impl RedisFacade {
     /// Creates a new `RedisFacade`, owning an active `redis` `Connection`
     pub fn new(redis: Connection) -> RedisFacade {
-        RedisFacade { redis }
+        RedisFacade(redis)
     }
 
     pub fn get_string(&self, key: &str) -> RedisResult<String> {
-        self.redis.get::<_, String>(key)
+        self.0.get::<_, String>(key)
     }
 
     pub fn get_bool(&self, key: &str) -> RedisResult<bool> {
-        self.redis.get::<_, bool>(key)
+        self.0.get::<_, bool>(key)
     }
 
     pub fn exists(&self, key: &str) -> RedisResult<bool> {
-        self.redis.exists::<_, bool>(key)
+        self.0.exists::<_, bool>(key)
     }
 
     pub fn increment(&self, key: &str) -> RedisResult<i64> {
-        self.redis.incr::<_, _, i64>(key, 1)
+        self.0.incr::<_, _, i64>(key, 1)
     }
 
     pub fn expire(&self, key: &str, period: usize) -> RedisResult<()> {
-        self.redis.expire::<_, ()>(key, period)
+        self.0.expire::<_, ()>(key, period)
     }
 
     pub fn set(&self, key: &str, value: &str) -> RedisResult<()> {
-        self.redis.set::<_, _, ()>(key, value)
+        self.0.set::<_, _, ()>(key, value)
     }
 }
